eec1bd786cb072a495c55e6014ac875280d529e4
Sam Thompson <electronic62794@yahoo.com>
2019-04-24 10:11:42 -0700

    first commit

---
+++ Unsolved/client/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+    
+      
+        <div>
+          <Nav />
+          <div>
+            <Switch>
+            <Route exact path="/" component={Books} />
+            <Route exact path="/books" component={Books} />
+            <Route exact path="/books/:id" component={Detail} />
+            <Route component={NoMatch} />
+            </Switch>
+          </div>
+        </div>
+        
+      </Router>
+  );
+}
+
+export default App;
